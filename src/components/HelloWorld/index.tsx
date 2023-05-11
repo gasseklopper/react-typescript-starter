0cc480643c24c5824cd304a4a06da2d5e499e115
Markus Morley <markus.morley@sinnerschrader.com>
2023-05-11 11:32:19 +0200

    feat: add react-dom-router

--- src/components/HelloWorld/index.tsx
+++ src/components/HelloWorld/index.tsx
@@ -1,11 +1,32 @@
 import React from "react";
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 
 const HelloWorld: React.FC = () => (
     <>
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">About</Link>
+                        </li>
+                        <li>
+                            <Link to="/users">Users</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <Routes>
+                <Route path="/about" element={<About />} />
+                <Route path="/users" element={<Users />} />
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </Router>
         <h1>Hello World</h1>
-
         <hr />
-
         <h3>Environmental variables:</h3>
         <p>
             process.env.PRODUCTION: <b>{process.env.PRODUCTION.toString()}</b>
@@ -18,5 +39,29 @@ const HelloWorld: React.FC = () => (
         </p>
     </>
 );
+
+function Home() {
+    return (
+        <>
+            <h2>Home</h2>
+        </>
+    );
+}
+
+function About() {
+    return (
+        <>
+            <h2>About</h2>
+        </>
+    );
+}
+
+function Users() {
+    return (
+        <>
+            <h2>Users</h2>
+        </>
+    );
+}
 
 export default HelloWorld;
